0ea5c0ceb5d8bf1fc9a9f5168bc86b0c4ab39271
GardenerGeralt <mikemaximchuk@gmail.com>
2024-10-28 15:35:52 +0100

    added image preview and a shortcut to open files with tab

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,20 +1,36 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-        vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 
-        local config = require("neo-tree")
-        config.setup({
-            window = {
-                width = 25,
-            },
-        })
-    end,
+		local config = require("neo-tree")
+		config.setup({
+			window = {
+				width = 25,
+			},
+			filesystem = {
+				window = {
+					mappings = {
+						["<leader>p"] = "image_wezterm",
+                        ["<tab>"] = "open",
+					},
+				},
+				commands = {
+					image_wezterm = function(state)
+						local node = state.tree:get_node()
+						if node.type == "file" then
+							require("image_preview").PreviewImage(node.path)
+						end
+					end,
+				},
+			},
+		})
+	end,
 }
