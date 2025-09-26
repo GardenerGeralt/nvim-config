db2067ee82fa440e16b6bc42d4f11d04826177a7
GardenerGeralt <mikemaximchuk@gmail.com>
2025-09-26 19:05:40 +0200

    removed devcontainer-cli and catppuccin. fixed lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,38 +1,39 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright", "rust_analyzer" },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright", "rust_analyzer" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilites = capabilities,
-            })
-            lspconfig.pyright.setup({
-                capabilites = capabilities,
-            })
-            lspconfig.rust_analyzer.setup({
-                capabilites = capabilities,
-            })
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
-        end,
-    },
+			vim.lsp.config("lua_ls", {
+			    capabilites = capabilities,
+			})
+			vim.lsp.config("pyright", {
+			    capabilites = capabilities,
+			})
+			vim.lsp.config("rust_analyzer", {
+			    capabilites = capabilities,
+			})
+			-- vim.lsp.enable("lua_ls")
+			-- vim.lsp.enable("pyright")
+			-- vim.lsp.enable("rust_analyzer")
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
+		end,
+	},
 }
